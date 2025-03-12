1b53e48dcbb106ba2810fc4ba861df04d69ba97c
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-03-12 20:15:07 +0530

    DOM Challange 2 completed

--- JS/DOMChallanges/Challanges1-5/challenge-2/app.js
+++ JS/DOMChallanges/Challanges1-5/challenge-2/app.js
@@ -1,3 +1,13 @@
-/**
- * Write your challenge solution here
- */
+const mainHeading = document.getElementById('mainHeading')
+const colorButtons = document.querySelector(".color-buttons")
+
+colorButtons.childNodes.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        if(e.target.innerText === 'Reset'){
+            mainHeading.style.color = ''
+            return
+        }
+        mainHeading.style.color = e.target.innerText
+    })
+});
+
