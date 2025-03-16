8d386d7ceb206ed7f630ebcc8f36c5f047ae4cc5
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-03-16 17:02:03 +0530

    DOM Challange 9 Completed

--- JS/DOMChallanges/Challanges6-10/challenge-9/app.js
+++ JS/DOMChallanges/Challanges6-10/challenge-9/app.js
@@ -1,3 +1,38 @@
-/**
- * Write your challenge solution here
- */
+const togglebtn = document.querySelector('.toggle-btn')
+const panel = document.querySelector('.panel')
+const closebtn = document.querySelector('.close-btn')
+const menuItems = document.querySelectorAll('.menu-item')
+
+let isOpen = false
+
+togglebtn.addEventListener('click', togglePanel)
+
+closebtn.addEventListener('click', togglePanel)
+
+document.addEventListener('click', (e) => handleDocClick(e))
+
+menuItems.forEach((item) => {
+    item.addEventListener('click', () => alert(item.textContent))
+})
+
+function togglePanel() {
+    if(!isOpen){
+        panel.style.right = '0'
+        isOpen = true
+    } else {
+        panel.style.right = '-360px'
+        isOpen = false
+    }
+}
+
+function handleDocClick(e) {
+    if(isOpen && e.target !== togglebtn){
+        if(e.target === panel || e.target.parentNode === panel) {
+            return
+        }else {
+            panel.style.right = '-360px'
+            isOpen = false
+        }
+    }
+    
+}
