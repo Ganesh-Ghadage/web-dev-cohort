e015592a44bb4b959b9df0a721d35f045318cad6
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-03-16 14:31:29 +0530

    DOM Challange 7 Completed

--- JS/DOMChallanges/Challanges6-10/challenge-7/app.js
+++ JS/DOMChallanges/Challanges6-10/challenge-7/app.js
@@ -1,3 +1,19 @@
-/**
- * Write your challenge solution here
- */
+const accordionItems = document.querySelectorAll('.accordion-item')
+
+accordionItems.forEach((item, idx) => {
+    item.childNodes[1].addEventListener('click', () => handleClick(idx))
+    
+})
+
+function handleClick(index) {
+
+    accordionItems.forEach((item, idx) => {
+        if(index === idx) {
+            item.classList.contains('active') ? item.classList.remove('active') : item.classList.add('active')
+        }else {
+            item.classList.remove('active')
+        }
+    })
+
+    
+}
