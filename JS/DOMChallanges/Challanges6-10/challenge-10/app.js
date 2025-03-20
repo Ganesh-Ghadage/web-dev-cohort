a50d479687e3a12e85cc295f82eed7f3434b796e
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-03-20 22:12:41 +0530

    DOM Challange 10 Completed

--- JS/DOMChallanges/Challanges6-10/challenge-10/app.js
+++ JS/DOMChallanges/Challanges6-10/challenge-10/app.js
@@ -1,22 +1,175 @@
-/**
- * Write your challenge solution here
- */
-// Image data
-const images = [
+
+const emojis = [
   {
-    url: 'https://plus.unsplash.com/premium_photo-1666863909125-3a01f038e71f?q=80&w=1986&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    caption: 'Beautiful Mountain Landscape',
+    emoji: '😺',
+    caption: 'cat'
   },
   {
-    url: 'https://plus.unsplash.com/premium_photo-1690576837108-3c8343a1fc83?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    caption: 'Ocean Sunset View',
+    emoji: '🐭',
+    caption: 'mouse'
   },
   {
-    url: 'https://images.unsplash.com/photo-1473448912268-2022ce9509d8?q=80&w=2041&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    caption: 'Autumn Forest Path',
+    emoji: '🐵',
+    caption: 'monkey'
   },
   {
-    url: 'https://plus.unsplash.com/premium_photo-1680466057202-4aa3c6329758?q=80&w=2138&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    caption: 'Urban City Skyline',
+    emoji: '🦊',
+    caption: 'fox'
+  },
+  {
+    emoji: '🐼',
+    caption: 'panda'
+  },
+  {
+    emoji: '🐶',
+    caption: 'dog'
+  },
+  {
+    emoji: '🐮',
+    caption: 'cow'
+  },
+  {
+    emoji: '🦁',
+    caption: 'lion'
   },
 ];
+
+const movesDiaplay = document.getElementById('moves')
+const timeDiaplay = document.getElementById('time')
+const gameContainer = document.getElementById('gameContainer')
+
+const winDisplay = document.createElement('p')
+
+let seconds = 0;
+let minutes = 0;
+let timerIdx = null
+let prevIds = []
+let currentFlippedCards = []
+let checkedCards = []
+let moves = 0
+
+function startGame() {
+
+  winDisplay.textContent = ''
+
+  displayCards()
+
+  timerIdx = setInterval(() => {
+    seconds++
+    if(seconds === 60) {
+      minutes++
+      seconds = 0
+    }
+    timeDiaplay.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`
+  }, 1000)
+}
+
+function displayCards() {
+  for (let i = 0; i < 16; i++){
+    const card = document.createElement('div')
+    card.classList.add('card')
+  
+    const cardFront = document.createElement('div')
+    cardFront.classList.add('card-front')
+    cardFront.textContent = '?'
+    
+    const cardBack = document.createElement('div')
+    cardBack.classList.add('card-back')
+  
+    const randomIdx = generateRandomId()
+    prevIds.push(randomIdx)
+    cardBack.textContent = emojis[randomIdx].emoji
+    
+    card.setAttribute("data", emojis[randomIdx].caption)
+    
+    card.appendChild(cardFront)
+    card.appendChild(cardBack)
+  
+    card.onclick = () => cardClick(card)
+  
+    gameContainer.appendChild(card)
+  }
+}
+
+function cardClick(card) {
+  const cards = document.querySelectorAll('.card')
+
+  if(currentFlippedCards.length >= 2) {
+    cards.forEach(item => {
+      if(!checkedCards.includes(item)){
+        item.classList.remove('flipped')
+      }
+    });
+    currentFlippedCards = []
+  }
+
+  flipCard(card)
+
+  if(currentFlippedCards.length >= 2) {
+    moves++
+    movesDiaplay.textContent = moves
+    if(checkCard()) {
+      checkedCards.push(...currentFlippedCards)
+    }
+  }
+
+  checkWin()
+
+}
+
+function flipCard(card) {
+  if(card.classList.contains('flipped')){
+    card.classList.remove('flipped')
+    currentFlippedCards = currentFlippedCards.filter((item) => item !== card)
+  } else {
+    card.classList.add('flipped')
+    currentFlippedCards.push(card)
+  }
+}
+
+function checkCard() {
+  return currentFlippedCards[0].attributes.data.value === currentFlippedCards[1].attributes.data.value
+}
+
+function checkWin() {
+  if(checkedCards.length === 16){
+    clearInterval(timerIdx)
+    
+    winDisplay.textContent = `You won a Game with ${moves} moves in ${minutes} min ${seconds.toString().padStart(2, '0')} sec`
+    winDisplay.style.fontSize = '2em'
+    winDisplay.style.color = 'green'
+    document.body.appendChild(winDisplay)
+  }
+}
+
+function restartGame() {
+  gameContainer.innerHTML = ''
+  clearInterval(timerIdx)
+  seconds = 0;
+  minutes = 0;
+  timerIdx = null
+  prevIds = []
+  currentFlippedCards = []
+  checkedCards = []
+  moves = 0
+
+  timeDiaplay.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`
+  movesDiaplay.textContent = moves
+
+  startGame()
+}
+
+function generateRandomId() {
+  let randomId = Math.floor(Math.random() * emojis.length)
+
+  if(prevIds.includes(randomId)) {
+    if(prevIds.length === emojis.length) {
+      prevIds = []
+    }
+    return generateRandomId()
+  }
+
+  return randomId
+}
+
+// startGame()
