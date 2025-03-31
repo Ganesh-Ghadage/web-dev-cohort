5d0c3296b0f5ea7c87b1ce22ab8dc83750acd002
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-03-31 21:08:16 +0530

    Add controllers for authentication, health check, notes, projects, and tasks

---
+++ ProjectManagemnetMegaProject/src/controllers/healthcheck.controllers.js
@@ -0,0 +1,10 @@
+import { ApiResponce } from "../utils/api-responce.js"
+
+const healthCheck = (req, res) => {
+    return res.status(200).json(
+        new ApiResponce(200, 
+        { messae: "Server is up and OK "})
+    )
+}
+
+export { healthCheck }
