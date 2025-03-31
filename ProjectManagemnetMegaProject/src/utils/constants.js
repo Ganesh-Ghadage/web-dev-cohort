7e93f9ece6340578d569d81bcfc0dd3c8f966030
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-03-31 21:07:56 +0530

    Add utility functions and constants for task management and error handling

---
+++ ProjectManagemnetMegaProject/src/utils/constants.js
@@ -0,0 +1,15 @@
+export const userRoleEnums = {
+    ADMIN: "admin",
+    PROJECT_ADMIN: "project_admin",
+    MEMBER: 'member'
+}
+
+export const avaliableUserRoles = Object.values(userRoleEnums)
+
+export const taskStatusEnums = {
+    TODO: "todo",
+    IN_PROGRESS: "in_progress",
+    DONE: "done"
+}
+
+export const avaliableTaskStatus = Object.values(taskStatusEnums)
