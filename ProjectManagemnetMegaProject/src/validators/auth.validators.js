1c9e0142400ddd9fc9bc8e34a03e2f1e37887dc8
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-01 15:10:15 +0530

    Add validation middleware for user registration, notes, projects, and tasks

---
+++ ProjectManagemnetMegaProject/src/validators/auth.validators.js
@@ -0,0 +1,30 @@
+import { body } from 'express-validator'
+
+const userRegisterValidator = () => {
+  return [
+    body("email")
+      .trim()
+      .notEmpty().withMessage("Email is required")
+      .isEmail().withMessage("Not a valid Email"),
+    body("username")
+      .trim()
+      .notEmpty().withMessage("Username is required")
+      .isLength({min: 3}).withMessage("Username should be more than 3 char")
+      .isLength({max: 13}).withMessage("Username cannot exceed more than 13 char"),
+    body("password")
+      .trim()
+      .notEmpty().withMessage("Password is required")
+      .isLength({min: 8}).withMessage("Username should be more than 8 char")
+      .isLength({max: 20}).withMessage("Username cannot exceed more than 20 char")
+      .isStrongPassword({minUppercase: 1, minNumbers: 2,  minSymbols: 1}).withMessage("Password should contain at least 1 Uppcase letter, 1 Symbol and at least 2 number"),
+    body("role")
+      .trim()
+      .default('member'),
+    body("fullname")
+      .trim()
+      .notEmpty().withMessage("User full name is required")
+  ]
+}
+
+
+export { userRegisterValidator }
