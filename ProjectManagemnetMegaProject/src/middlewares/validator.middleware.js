49e83660559aff0cf1c069f02582388e9da6902f
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-04-03 19:53:46 +0530

    Enhance validation middleware to throw ApiError for invalid data and add username uniqueness check in registration validator

--- ProjectManagemnetMegaProject/src/middlewares/validator.middleware.js
+++ ProjectManagemnetMegaProject/src/middlewares/validator.middleware.js
@@ -1,11 +1,25 @@
 import { validationResult } from "express-validator";
+import { ApiError } from "../utils/api-error.js"
 
 const validate = (req, res, next) => {
   const error = validationResult(req)
 
-  console.log(error)
+  if(error.isEmpty()) {
+    return next()
+  }
 
-  next()
+  // console.log(error)
+
+  const extractedErrors = []
+  error.array().map(err => {  
+    extractedErrors.push({
+      [err.path]: err.msg
+    })
+  })
+  
+  // console.log(extractedErrors)
+  
+  throw new ApiError(422, "Data is not valid", extractedErrors)
 }
 
 export default validate
--- ProjectManagemnetMegaProject/src/validators/auth.validators.js
+++ ProjectManagemnetMegaProject/src/validators/auth.validators.js
@@ -1,4 +1,5 @@
 import { body } from 'express-validator'
+import { User } from '../models/user.models.js'
 
 const userRegisterValidator = () => {
   return [
@@ -10,16 +11,23 @@ const userRegisterValidator = () => {
       .trim()
       .notEmpty().withMessage("Username is required")
       .isLength({min: 3}).withMessage("Username should be more than 3 char")
-      .isLength({max: 13}).withMessage("Username cannot exceed more than 13 char"),
+      .isLength({max: 13}).withMessage("Username cannot exceed more than 13 char")
+      .custom(async (username) => {
+        const existingUser = await User.findOne({username})
+
+        if(existingUser) {
+          throw new Error("Username is taken")
+        }
+      }),
     body("password")
       .trim()
       .notEmpty().withMessage("Password is required")
       .isLength({min: 8}).withMessage("Username should be more than 8 char")
       .isLength({max: 20}).withMessage("Username cannot exceed more than 20 char")
       .isStrongPassword({minUppercase: 1, minNumbers: 2,  minSymbols: 1}).withMessage("Password should contain at least 1 Uppcase letter, 1 Symbol and at least 2 number"),
-    body("role")
-      .trim()
-      .default('member'),
+    // body("role")
+    //   .trim()
+    //   .default('member'),
     body("fullname")
       .trim()
       .notEmpty().withMessage("User full name is required")
