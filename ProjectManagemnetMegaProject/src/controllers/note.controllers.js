0e9a2b658d76588e5f12812830baeb77844a26fb
Ganesh Ghadage <ganeshvg22@gmail.com>
2025-05-01 11:28:10 +0530

    feat: implement get all notes functionality with authentication

--- ProjectManagemnetMegaProject/src/controllers/note.controllers.js
+++ ProjectManagemnetMegaProject/src/controllers/note.controllers.js
@@ -1,8 +1,19 @@
-// boilderplate code
+import { asyncHandler } from "../utils/async-handler.js";
+import { ApiResponce } from "../utils/api-responce.js";
+import { ApiError } from "../utils/api-error.js";
+import { ProjectNote } from "../models/note.models.js";
 
-const getNotes = async (req, res) => {
-  // get all notes
-};
+const getNotes = asyncHandler(async (req, res) => {
+  const notes = await ProjectNote.find();
+
+  if (!notes) {
+    throw new ApiError(404, "Notes for found");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponce(200, notes, "All notes fetched successfully"));
+});
 
 const getNoteById = async (req, res) => {
   // get note by id
--- ProjectManagemnetMegaProject/src/routes/note.routes.js
+++ ProjectManagemnetMegaProject/src/routes/note.routes.js
@@ -1,5 +1,12 @@
 import { Router } from "express"
+import authenticateUser from "../middlewares/auth.middleware.js";
+import { getNotes } from "../controllers/note.controllers.js";
 
 const router = Router()
+
+router.route('/get-all-notes').get(
+  authenticateUser,
+  getNotes
+)
 
 export default router
